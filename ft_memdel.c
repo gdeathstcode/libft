ae671c7aa174479647a75b2cb487c0b6f1a7a993
Group Deathstroke <gdeathst@mi-n5.msk.21-school.ru>
2022-03-27 17:40:35 +0300

    update library: standart libft, get_next_line, ft_printf

--- ft_memdel.c
+++ ft_memdel.c
@@ -1,35 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strpbrk.c                                       :+:      :+:    :+:   */
+/*   ft_memdel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gdeathst <gdeathst@student.21-school.ru>   +#+  +:+       +#+        */
+/*   By: gdeathst <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/19 16:17:18 by gdeathst          #+#    #+#             */
-/*   Updated: 2021/12/19 16:17:19 by gdeathst         ###   ########.fr       */
+/*   Created: 2022/01/19 18:17:54 by gdeathst          #+#    #+#             */
+/*   Updated: 2022/01/19 18:19:07 by gdeathst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strpbrk(const char *s, const char *charset)
+void	ft_memdel(void **ap)
 {
-	const char	*scanp;
-	int			c;
-	int			sc;
-
-	while (*s != 0)
+	if (ap != NULL)
 	{
-		c = *s;
-		scanp = charset;
-		while (*scanp != 0)
-		{
-			sc = *scanp;
-			if (sc == c)
-				return (((char *) s));
-			scanp++;
-		}
-		s++;
+		free(*ap);
+		*ap = NULL;
 	}
-	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,26 +6,21 @@
 /*   By: gdeathst <gdeathst@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 06:46:56 by gdeathst          #+#    #+#             */
-/*   Updated: 2021/12/26 06:46:57 by gdeathst         ###   ########.fr       */
+/*   Updated: 2022/01/19 18:20:29 by gdeathst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define FILEDESCRIPTORS 256
-
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
-# include <stddef.h>
 # include <stdlib.h>
-# include <limits.h>
+# include "libft.h"
+
+# define BUFFER_SIZE 1024
 
 char	*get_next_line(int fd);
-
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-
 
 #endif
